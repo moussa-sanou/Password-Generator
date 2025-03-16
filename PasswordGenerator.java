dbecced9112ff497cfe846961547c74d9b834749
samidollars <muss.junior@gmail.com>
2025-03-16 00:04:14 -0500

    Project: Password Generator Complete project

--- PasswordGenerator.java
+++ PasswordGenerator.java
@@ -7,12 +7,36 @@ public class PasswordGenerator {
                             boolean useNumbers, boolean useSpecialChars)
     {
         String lowercase = "abcdefghijklmnoprstuvwxyz";
+        String uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        String digits = "0123456789";
+        String symbols = "!@#$%^&*()?";
+
+        StringBuilder charOptions = new StringBuilder();
+
+        if (useLowerCase){
+            charOptions.append(lowercase);
+        }
+
+        if (useUpperCase){
+            charOptions.append(uppercase);
+        }
+
+        if (useNumbers){
+            charOptions.append(digits);
+        }
+
+        if (useSpecialChars){
+            charOptions.append(symbols);
+        }
+
+        String allChars = charOptions.toString();
+
         StringBuilder password = new StringBuilder();
         Random rand = new Random();
 
         for (int i = 0; i < length; i++)
         {
-            char nextLetter = lowercase.charAt(rand.nextInt(lowercase.length()));
+            char nextLetter = allChars.charAt(rand.nextInt(allChars.length()));
             password.append(nextLetter);
         }
 
